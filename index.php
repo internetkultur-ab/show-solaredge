ae8cf8bbfe9fa17e4d6a852314a220bdb087593d
Alex Nilsson <alex@internetkultur.se>
2023-08-31 17:12:26 +0200

    avrundning

--- index.php
+++ index.php
@@ -34,10 +34,10 @@ $data = getData($_GET['key'], $_GET['site']);
   </head>
   <body>
     <main class="container">
-      <p>Produktion just nu: <br><?php echo $data['overview']['currentPower']['power'] / 1000 ; ?> kW</p>
-      <p>Dagens produktion: <br><?php echo $data['overview']['lastDayData']['energy'] / 1000; ?> kWh</p>
-      <p>Månadens produktion: <br><?php echo $data['overview']['lastMonthData']['energy'] / 1000000; ?> MWh</p>
-      <p>Sedan installation: <br><?php echo $data['overview']['lifeTimeData']['energy'] / 1000000; ?> MWh</p>
+      <p>Produktion just nu: <br><?php echo round($data['overview']['currentPower']['power'] / 1000, 2) ; ?> kW</p>
+      <p>Dagens produktion: <br><?php echo round($data['overview']['lastDayData']['energy'] / 1000, 2); ?> kWh</p>
+      <p>Månadens produktion: <br><?php echo round($data['overview']['lastMonthData']['energy'] / 1000000, 2); ?> MWh</p>
+      <p>Sedan installation: <br><?php echo round($data['overview']['lifeTimeData']['energy'] / 1000000, 2); ?> MWh</p>
       <p>Senast uppdaterad: <br><?php echo $data['overview']['lastUpdateTime']; ?></p>
     </main>
   </body>
