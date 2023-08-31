999af4b6c2fa49f08c1c2f3383c2dc640f3a8ff9
Alex Nilsson <alex@internetkultur.se>
2023-08-31 16:48:23 +0200

    första version!

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!isset($_GET['key'])) or (empty($_GET['key'])) or (!isset($_GET['site'])) or (empty($_GET['site'])) {
+    die();
+}
+
+
+function getData($key, $site) {
+    $url = "https://monitoringapi.solaredge.com/site/" . $site . "/overview?api_key=". $key ."";
+    $options = [
+        "http" => [
+            "header" => "accept: application/json",
+            "method" => "GET",
+            "content" => "",
+        ],
+    ];
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    $data = json_decode($result, true);
+    return $data;
+
+}
+
+print_r($data);
+
+?>
