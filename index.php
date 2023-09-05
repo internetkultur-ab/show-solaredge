e01391473363701a86c1829d320509f5238a4d47
Alex Nilsson <alex@internetkultur.se>
2023-09-05 21:18:27 +0200

    stylingfix

--- index.php
+++ index.php
@@ -26,27 +26,38 @@ $data = getData($_GET['key'], $_GET['site']);
 ?>
 <!doctype html>
 <html lang="sv">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./style.css">
-    <title>Solaredge</title>
-  </head>
-  <body>
 
-    <div class="parent">
-<div class="div1 box"><p><span class="key">Aktuell effekt</span><br>
-<span class="value">
-<?php echo number_format($data['overview']['currentPower']['power'] / 1000, 2, ",", "") ; ?><br>kW</span></p></div>
-<div class="div2 box"><p><span class="key">Energi idag</span><br>
-<span class="value">
-<?php echo number_format($data['overview']['lastDayData']['energy'] / 1000, 2, ",", ""); ?><br>kWh</span></p></div>
-<div class="div3 box"><p><span class="key">Månadsenergi</span><br>
-<span class="value">
-<?php echo number_format($data['overview']['lastMonthData']['energy'] / 1000000, 2, ",", ""); ?><br>MWh</span></p></div>
-<div class="div4 box"><p><span class="key">Total energi</span><br>
-<span class="value">
-<?php echo number_format($data['overview']['lifeTimeData']['energy'] / 1000000, 2, ",", ""); ?><br>MWh</span></p></div>
-</div>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./style.css">
+  <title>Solaredge</title>
+</head>
+
+<body>
+
+  <div class="parent">
+    <div class="div1 box">
+      <p class="key">Aktuell effekt</p>
+      <p class="value1"><?php echo number_format($data['overview']['currentPower']['power'] / 1000, 2, ",", "") ; ?></p>
+      <p class="value2">kW</p>
+    </div>
+    <div class="div2 box">
+      <p class="key">Energi idag</p>
+      <p class="value1"><?php echo number_format($data['overview']['lastDayData']['energy'] / 1000, 2, ",", ""); ?></p>
+      <p class="value2">kWh</p>
+    </div>
+    <div class="div3 box">
+      <p class="key">Månadsenergi</p>
+      <p class="value1"><?php echo number_format($data['overview']['lastMonthData']['energy'] / 1000000, 2, ",", ""); ?></p>
+      <p class="value2">MWh</p>
+    </div>
+    <div class="div4 box">
+      <p class="key">Total energi</p>
+      <p class="value1"><?php echo number_format($data['overview']['lifeTimeData']['energy'] / 1000000, 2, ",", ""); ?></p>
+      <p class="value2">MWh</p>
+    </div>
+  </div>
+</body>
+
 </html>
