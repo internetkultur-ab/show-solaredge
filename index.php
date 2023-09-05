b5ed988c8b0c5d6151bc4179ef712f2f28f6df97
Alex Nilsson <alex@internetkultur.se>
2023-09-05 16:23:59 +0200

    ny style

--- index.php
+++ index.php
@@ -29,16 +29,24 @@ $data = getData($_GET['key'], $_GET['site']);
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/picocss/1.5.2/pico.min.css" integrity="sha512-3gFq2IXMVlAQaUyahzhjDRivv0yqyXTb7xiy6ivTaG5Uz4hKI54uYxpQefdomgDVQ11eJVUbXG0YdPMDISiGgg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
     <title>Solaredge</title>
   </head>
   <body>
-    <main class="container">
-      <p>Produktion just nu: <br><?php echo number_format($data['overview']['currentPower']['power'] / 1000, 2, ",", "") ; ?> kW</p>
-      <p>Dagens produktion: <br><?php echo number_format($data['overview']['lastDayData']['energy'] / 1000, 2, ",", ""); ?> kWh</p>
-      <p>Månadens produktion: <br><?php echo number_format($data['overview']['lastMonthData']['energy'] / 1000000, 2, ",", ""); ?> MWh</p>
-      <p>Sedan installation: <br><?php echo number_format($data['overview']['lifeTimeData']['energy'] / 1000000, 2, ",", ""); ?> MWh</p>
-      <p>Senast uppdaterad: <br><?php echo $data['overview']['lastUpdateTime']; ?></p>
-    </main>
+
+    <div class="parent">
+<div class="div1 box"><p><span class="key">Aktuell effekt</span><br>
+<span class="value">
+<?php echo number_format($data['overview']['currentPower']['power'] / 1000, 2, ",", "") ; ?><br>kW</span></p></div>
+<div class="div2 box"><p><span class="key">Energi idag</span><br>
+<span class="value">
+<?php echo number_format($data['overview']['lastDayData']['energy'] / 1000, 2, ",", ""); ?><br>kWh</span></p></div>
+<div class="div3 box"><p><span class="key">Månadensenergi</span><br>
+<span class="value">
+<?php echo number_format($data['overview']['lastMonthData']['energy'] / 1000000, 2, ",", ""); ?><br>MWh</span></p></div>
+<div class="div4 box"><p><span class="key">Total energi</span><br>
+<span class="value">
+<?php echo number_format($data['overview']['lifeTimeData']['energy'] / 1000000, 2, ",", ""); ?><br>MWh</span></p></div>
+</div>
   </body>
 </html>
